cd0f68d0c7fa2a4243f149fac06c78f0d401c21b
just-hoshinon <mwadman2018@gmail.com>
2023-03-12 20:13:39 +0200

    feature: added first part of tests for List

---
+++ src/List.test.ts
@@ -0,0 +1,143 @@
+import { List } from './List';
+
+describe('Testing first implementation of List', () => {
+
+  test('Check adding an element', () => {
+    const list = new List();
+
+    list.append('A');
+    const result = list.arr[0];
+
+    expect(result).toBe('A');
+  });
+
+  test('Check getting an element', () => {
+    const list = new List();
+
+    list.append('A');
+    const result = list.get(0);
+
+    expect(result).toBe('A');
+  });
+
+  test('Check length of a list', () => {
+    const list = new List();
+
+    list.append('A');
+    list.append('A');
+    list.append('A');
+    const result = list.length();
+
+    expect(result).toBe(3);
+  });
+
+  test('Check deleting by index', () => {
+    const list = new List();
+
+    list.append('A');
+    const value = list.delete(0);
+    const result = list.arr[0];
+
+    expect(value).toBe('A');
+    expect(result).toBe(undefined);
+
+    try {
+      list.delete(-1);
+    } catch (e: any) {
+      expect(e.message).toBe('Invalid number')
+    }
+
+    try {
+      list.delete(list.length() + 1);
+    } catch (e: any) {
+      expect(e.message).toBe('Invalid number')
+    }
+  });
+
+  test('Check deleting by value', () => {
+    const list = new List();
+
+    list.append('A');
+    list.append('B');
+    list.append('B');
+    list.append('A');
+    list.append('C');
+    list.append('D');
+    list.append('A');
+
+    list.deleteAll('A');
+    const index = list.arr.indexOf('A');
+
+    expect(index).toBe(-1);
+  });
+
+  test('Check appending a list to an end', () => {
+    const list = new List();
+
+    list.append('A');
+    list.append('B');
+    list.append('B');
+    list.append('A');
+    list.extend(['B', 'C', 'D', 'A']);
+
+    expect(list.arr[4]).toBe('B');
+    expect(list.arr[5]).toBe('C');
+    expect(list.arr[6]).toBe('D');
+    expect(list.arr[7]).toBe('A');
+  });
+
+  test('Check finding a first index by value', () => {
+    const list = new List();
+
+    list.append('B');
+    list.append('A');
+    list.append('B');
+    list.append('C');
+    list.append('A');
+    const character = list.findFirst('A');
+
+    expect(character).toBe(1);
+  });
+
+  test('Check finding a last index by value', () => {
+    const list = new List();
+
+    list.append('B');
+    list.append('A');
+    list.append('B');
+    list.append('C');
+    list.append('A');
+    const character = list.findLast('A');
+
+    expect(character).toBe(4);
+  });
+
+  test('Check inserting an element', () => {
+    const list = new List();
+
+    list.append('B');
+    list.append('A');
+    list.append('B');
+    list.append('C');
+    list.insert('D', 2);
+    const character = list.arr[2];
+
+    expect(character).toBe('D');
+  });
+
+  test('Check cloning a list', () => {
+    const list = new List();
+
+    list.append('B');
+    list.append('A');
+    list.append('B');
+    list.append('C');
+    const newList = list.clone();
+
+    expect(list).not.toBe(newList);
+    newList.arr.forEach((value, index) => {
+      expect(value).toBe(list.arr[index]);
+    })
+  });
+
+});
